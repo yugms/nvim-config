acea83b891148ee73106d24b49862381e38fd51d
yugm shah <yugsha28@gmail.com>
2025-01-24 18:14:39 -0500

    add options.lua with basic settings

---
+++ lua/config/options.lua
@@ -0,0 +1,37 @@
+-- indentation
+vim.o.shiftwidth = 4
+vim.o.softtabstop = 4
+vim.o.expandtab = true
+vim.o.smartindent = true
+
+-- line stuff
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.cursorline = true
+
+-- wrap
+vim.o.wrap = false
+vim.o.linebreak = true
+vim.o.sidescrolloff = 8
+vim.o.whichwrap = 'bs<>[]hl'
+
+-- file/window stuff
+vim.o.backup = false
+vim.o.undofile = true
+vim.o.completeopt = 'menuone,noselect'
+vim.o.pumheight = 10
+vim.o.fileencoding = 'utf-8'
+-- vim.o.showmode = false
+vim.o.termguicolors = true
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+vim.o.clipboard = "unnamedplus"
+vim.o.autoindent = true
+vim.o.updatetime = 250
+vim.o.timeoutlen = 750
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.g.have_nerd_font = true
+vim.opt.signcolumn = 'yes'
+vim.opt.runtimepath:remove '/usr/share/vim/vimfiles'
